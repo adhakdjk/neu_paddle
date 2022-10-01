1b738eab47f41b5d9fff2c433817f4fc053c259c
rreerr <1261903425@qq.com>
2022-10-01 22:04:13 +0800

    l

--- B组成员/22-高宇哲/code/week1/week1.py
+++ B组成员/22-高宇哲/code/week1/week1.py
@@ -1,1 +1,21 @@
-print('make global change!')
+list1 = [1, 2, 3, 4, 5 ]
+list2 = ["a", "b", "c", "d","e","f"]
+list3 = ['physics', 'chemistry', 1997, 2000]
+
+len(list1)
+
+print(list1[4])
+list3.append(5)
+print(list3)
+
+score=80
+if score<60:
+    print('不及格')
+else:
+    print('及格')
+
+print(1==2)
+print(1!=2)
+
+list1 = [1,2,3,4]
+list1[2]
